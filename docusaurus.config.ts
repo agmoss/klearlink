ba054401b16b5cf7d314722eecb8d831e8f9cb4d
Andrew Moss <andrew@m0ss.dev>
2024-11-05 15:55:26 -0700

    feat: Icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Clearlinc',
   tagline: 'Know Your Borrowers. Instantly.',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
   url: 'https://clearlinc.io',
   baseUrl: '/',
   organizationName: 'clearlinc',
